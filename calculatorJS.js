b0b0d1e35df448a431ba880aa8041dc23ae487ff
Megha Patel <meghs2512@gmail.com>
2021-04-06 08:52:57 -0400

    Changes for Calculator committed

---
+++ calculatorJS.js
@@ -0,0 +1,11 @@
+function calculateDist() {
+  var x1 = parseInt(document.getElementById("x1").value);
+  var y1 = parseInt(document.getElementById("y1").value);
+
+  var x2 = parseInt(document.getElementById("x2").value);
+  var y2 = parseInt(document.getElementById("y2").value);
+  var ele = document.getElementById("distanceResult");
+
+  var distance = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+  ele.innerHTML = distance;
+}
